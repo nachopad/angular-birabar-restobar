9062afd3f187e8982fbaabab157c20df1f01bc6d
Ignacio Agustin Padilla <nacho20002008@gmail.com>
2023-06-11 23:15:14 -0300

    se construye estructura base del proyecto final

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const {mongoose} = require('./database');
+var app = express();
+//middlewares
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+//Cargamos el modulo de direccionamiento de rutas
+
+//setting
+app.set('port', process.env.PORT || 3000);
+//starting the server
+app.listen(app.get('port'), () => {
+console.log(`Server started on port`, app.get('port'));
+});
