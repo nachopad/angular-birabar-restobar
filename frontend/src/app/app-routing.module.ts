98e2a494b0bc933b64da7a3edd23fc464ebe5461
Ignacio Agustin Padilla <nacho20002008@gmail.com>
2023-06-16 21:00:33 -0300

    se agrega funcion de login, componente de logueo y se realizan pequeñas correcciones en logica de usuario

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"principal",pathMatch:"full"},
+  { path: 'principal', component: HomeComponent},
+  { path: 'login', component: LoginComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
