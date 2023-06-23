8bcfc63bc1bb50f3ff8f937bc77753f7034492d3
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-06-23 17:36:19 -0300

    Oferta primer commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OfertaComponent } from './components/oferta/oferta.component';
+import { OfertaGestionComponent } from './components/oferta-gestion/oferta-gestion.component';
+import { OfertaFormComponent } from './components/oferta-form/oferta-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"oferta", component:OfertaComponent},
+  {path:"ofertaGestion", component:OfertaGestionComponent},
+  {path:"oferta-form/:id", component:OfertaFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfertaComponent } from './components/oferta/oferta.component';
+import { OfertaGestionComponent } from './components/oferta-gestion/oferta-gestion.component';
+import { OfertaFormComponent } from './components/oferta-form/oferta-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfertaComponent,
+    OfertaGestionComponent,
+    OfertaFormComponent
   ],
   imports: [
     BrowserModule,
