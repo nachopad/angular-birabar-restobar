b105df5e62eed0565e88ec63729e27fe07b698d1
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-06-28 20:42:17 -0300

    registro

--- frontend/src/app/services/oferta.service.ts
+++ frontend/src/app/services/oferta.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Oferta } from '../models/oferta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertaService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  registrarOferta(oferta: Oferta):Observable<any>{
+    let httpOptions = {
+      headers: new HttpHeaders({
+        "Content-type":"application/json"
+        }
+      ), 
+      params: new HttpParams()
+    };
+    let body = JSON.stringify(oferta);
+    console.log(body);
+    return this._http.post("http://localhost:3000/api/oferta/crearOferta", body , httpOptions);
+  }
+
 }
